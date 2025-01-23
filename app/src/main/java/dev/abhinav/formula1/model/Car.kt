0766c3a8a91c57afaac0d5e247d4f418b9a94b36
abhinavp403 <abhinavp403@gmail.com>
2025-01-22 19:24:13 -0500

    Fix lag on scrolling by skipping recompositions

--- app/src/main/java/dev/abhinav/formula1/model/Car.kt
+++ app/src/main/java/dev/abhinav/formula1/model/Car.kt
@@ -1,10 +1,12 @@
 package dev.abhinav.formula1.model
 
+import androidx.compose.runtime.Immutable
 import androidx.room.Entity
 import androidx.room.Ignore
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "car")
+@Immutable
 data class Car(
     @PrimaryKey val name: String,
     val image: Int,
--- app/src/main/java/dev/abhinav/formula1/model/Circuit.kt
+++ app/src/main/java/dev/abhinav/formula1/model/Circuit.kt
@@ -1,9 +1,11 @@
 package dev.abhinav.formula1.model
 
+import androidx.compose.runtime.Immutable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "circuit")
+@Immutable
 data class Circuit(
     @PrimaryKey val name: String,
     val imageRes: Int,
